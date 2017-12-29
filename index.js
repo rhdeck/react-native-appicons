a03b439e2fcb1ae52a28f774e5dc0833fb7135c9
Ray Deck <ray@raydeck.com>
2017-12-29 18:11:54 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+const cp = require("child_process");
+function getPackagePath() {
+  return process.cwd() + "/package.json";
+}
+function getPackage() {
+  const packagePath = getPackagePath();
+  if (!fs.existsSync(packagePath)) {
+    console.log(
+      "This does not appear to be a valid directory. Try from your project root"
+    );
+    process.exit(1);
+  }
+  var package = require(packagePath);
+  return package;
+}
+function writePackage(newPackage) {
+  fs.writeFileSync(getPackagePath(), JSON.stringify(newPackage, null, 2));
+}
+function saveAppIconPath(path) {
+  var package = getPackage();
+  package.appIcon = path;
+  writePackage(package);
+}
+module.exports = {
+  name: "setappicon",
+  description: "Identify the application icon URL or path for building",
+  func: (argv, config, args) => {
+    if (!argv || !argv[0]) {
+      //It would be nice to pre-validate a
+      inquirer
+        .prompt({
+          name: "pathorurl",
+          default: getPackage().appIcon ? getPackage().appIcon : null,
+          message:
+            "What is the path or URL of the image you would like to base your application icons on? \n(should be big, ideally at least 1024x1024)",
+          validate: answer => {
+            if (answer && answer.length) {
+              return true;
+            }
+          }
+        })
+        .then(answers => {
+          saveAppIconPath(answers.pathorurl);
+          cp.spawn(
+            "/usr/bin/env",
+            ["node", "./node_modules/.bin/react-native-make-appicons"],
+            { stdio: "inherit" }
+          );
+        });
+    } else {
+      saveAppIconPath(argv[0]);
+      cp.spawn(
+        "/usr/bin/env",
+        ["node", "./node_modules/.bin/react-native-make-appicons"],
+        {
+          stdio: "inherit"
+        }
+      );
+    }
+  }
+};
---
+++ linkicons.js
@@ -0,0 +1,163 @@
+#!/usr/bin/env node
+const fs = require("fs");
+const path = require("path");
+const cp = require("child_process");
+const cpp = require("child-process-promise");
+const glob = require("glob");
+const url = require("url");
+const tmp = require("tmp");
+const request = require("request");
+function resize(source, target, width, height) {
+  if (!height) height = width;
+  if (!source || !target || !width) {
+    console.log(
+      "Bad arguments passed to resize",
+      source,
+      target,
+      width,
+      height
+    );
+    return;
+  }
+  const geometryold = parseInt(width) + "x" + parseInt(height);
+  const geometry = geometryold + "^";
+  return new Promise((resolve, reject) => {
+    cpp
+      .spawn("/usr/bin/env", [
+        "convert",
+        source,
+        "-resize",
+        geometry,
+        "-gravity",
+        "center",
+        "-extent",
+        geometryold,
+        target
+      ])
+      .then(
+        () => {
+          resolve(target);
+        },
+        error => {
+          reject("Could not create the appropriate icon file", target, error);
+        }
+      );
+  });
+}
+var contentsJSON = null;
+function getContents() {
+  if (contentsJSON) return contentsJSON;
+  const cwd = fs.realpathSync(__dirname);
+  const contentsPath = path.join(cwd, "Contents.json");
+  if (fs.existsSync(contentsPath)) {
+    const str = fs.readFileSync(contentsPath);
+    contentsJSON = JSON.parse(str);
+    return contentsJSON;
+  } else {
+    return null;
+  }
+}
+var cachedImagePath = null;
+function getImagePath() {
+  if (cachedImagePath) return cachedImagePath;
+  const iosDir = path.join(process.cwd(), "ios");
+  const imageDir = glob.sync(path.join(iosDir, "*", "Images.xcassets"))[0];
+  if (!imageDir) return null;
+  const AppIconDir = path.join(imageDir, "AppIcon.appiconset");
+  if (!fs.existsSync(AppIconDir)) {
+    fs.mkdirSync(AppIconDir);
+  }
+  cachedImagePath = AppIconDir;
+  return cachedImagePath;
+}
+function getPackagePath() {
+  return process.cwd() + "/package.json";
+}
+function getPackage() {
+  const packagePath = getPackagePath();
+  if (!fs.existsSync(packagePath)) {
+    console.log(
+      "This does not appear to be a valid directory. Try running from your project root"
+    );
+    process.exit();
+  }
+  var package = require(packagePath);
+  return package;
+}
+var imagestart = null;
+function loadImage() {
+  if (imagestart) return imagestart;
+  imagestart = getPackage().appIcon;
+  if (!imagestart) {
+    console.log('There us no appicon specified. Run "react-native setappicon"');
+    process.exit();
+  }
+  uri = new url.URL(imagestart);
+  return new Promise((resolve, reject) => {
+    if (uri.protocol.length) {
+      //Get the damn file
+      const tmppath = tmp.fileSync().name;
+      const bn = path.basename(uri.pathname);
+      const p = tmppath + "_" + bn;
+      request.get(imagestart, { encoding: null }, (e, r, b) => {
+        if (e) {
+          reject("Got an error: " + e);
+          return;
+        }
+        if (b) {
+          fs.writeFileSync(p, b);
+          resolve(p);
+          return;
+        } else {
+          reject("There was no data there");
+        }
+      });
+    } else {
+      const realpath = fs.realpathSync(imagestart);
+      if (fs.existsSync(realpath)) {
+        resolve(realpath);
+      } else {
+        reject(imagestart + " does not appear to exist");
+      }
+    }
+  });
+  return imagestart;
+}
+var contents = getContents();
+if (!contents) {
+  console.log("Could not find Contents.json file, aborting");
+  process.exit();
+}
+loadImage().then(
+  imagepath => {
+    contents.images = contents.images.map(obj => {
+      const hw = obj.size.split("x");
+      const scale = parseInt(obj.scale.substring(0, obj.scale.length - 1));
+      const width = hw[0] * (scale ? scale : 1);
+      const height = hw[1] * (scale ? scale : 1);
+      if (!obj.filename)
+        obj.filename = "icon_" + obj.idiom + width + "x" + height + ".png";
+      target = path.join(getImagePath(), obj.filename);
+      try {
+        fs.unlinkSync(target);
+      } catch (err) {}
+      resize(imagepath, target, width, height).then(
+        target => {},
+        () => {
+          console.log(
+            "This module requires imagemagick to run. \nTo install on MacOS:\n port install imagemagick\n -OR-\n brew install imagemagick\n\nOn Linux, try aptitude:\n apt-get install imagemagick"
+          );
+          process.exit();
+        }
+      );
+      return obj;
+    });
+    const contentsPath = path.join(getImagePath(), "Contents.json");
+    fs.writeFileSync(contentsPath, JSON.stringify(contents, null, 2));
+    console.log("Successfully created iOS application icons");
+  },
+  errormessage => {
+    console.log("Could not load the starting image", errormessage);
+    pricess.exit();
+  }
+);
